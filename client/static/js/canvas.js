a33a93b92a6dbbd3489a5aa9d3eccebe0bc30485
rafaelurben <github@rafaelurben.ch>
2022-12-12 13:27:18 +0100

    Updated GameCanvas

--- client/static/js/canvas.js
+++ client/static/js/canvas.js
@@ -1,6 +1,23 @@
+F = 10; // scale factor
+
 class GameCanvas {
     constructor(canvas) {
         this.canvas = canvas;
         this.ctx = this.canvas.getContext('2d');
+
+        this.setSize();
+    }
+
+    setSize(width, height) {
+        this.canvas.width = (width || 40) * F;
+        this.canvas.height = (height || 20) * F;
+    }
+
+    drawLine(x1, y1, x2, y2, color) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(x1*F, y1*F);
+        this.ctx.lineTo(x2*F, y2*F);
+        this.ctx.strokeStyle = color;
+        this.ctx.stroke();
     }
 }

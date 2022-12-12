180ec1c419e533393c24fadcde0562461b0c7f70
rafaelurben <github@rafaelurben.ch>
2022-12-12 19:41:45 +0100

    Potential fix for canvas sizing

--- client/static/js/canvas.js
+++ client/static/js/canvas.js
@@ -1,63 +1,109 @@
-F = 100; // scale factor
-
 class GameCanvas {
-    constructor(canvas) {
+    constructor(canvas, container) {
         this.canvas = canvas;
+        this.container = container;
         this.ctx = this.canvas.getContext('2d');
 
-        this.setSize();
-        this.drawCoordinateSystem();
+        // Virtual canvas size (in game units)
+        this.v_width = 30;
+        this.v_height = 15;
+
+        window.addEventListener('load', () => this.resize());
+        window.addEventListener('resize', () => this.resize());
     }
 
     clear() {
+        // clear canvas
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
-    setSize(width, height) {
-        let w = width || 30;
-        let h = height || 15;
-        this.width = w;
-        this.height = h;
-        this.canvas.width = w * F;
-        this.canvas.height = h * F;
+    resize() {
+        // Reset canvas size so the container has the correct dimensions
+        this.canvas.style.height = "1px";
+        this.canvas.style.width = "1px";
+
+        // Calculate real canvas size
+        let { width, height } = this.container.getBoundingClientRect();
+        const dpr = window.devicePixelRatio || 1;
+
+        // Apply better orientation
+        if (width/height >= this.v_width/this.v_height) {
+            width = height * this.v_width / this.v_height;
+        } else {
+            height = width * this.v_height / this.v_width;
+        }
+
+        // Apply device pixel ratio and round up
+        width = Math.ceil(width);
+        height = Math.ceil(height);
+        
+        // Set canvas size (in whole pixels)
+        this.canvas.width = width;
+        this.canvas.height = height;
+        
+        // Set canvas display size (in CSS pixels, can be fractional)
+        this.canvas.style.width = `${width}px`;
+        this.canvas.style.height = `${height}px`;
+
+        // Set canvas scale
+        //this.ctx.scale(dpr, dpr);
+
+        // Cleanup
+        this.clear();
+        this.drawCoordinateSystem();
+    }
+
+    x(x) {
+        // convert virtual x to canvas x
+        return (this.canvas.width / this.v_width) * x;
+    }
+
+    y(y) {
+        // convert virtual y to canvas y
+        return (this.canvas.height / this.v_height) * y;
+    }
+
+    c(x, y) {
+        // convert virtual (x,y) to canvas (x,y)
+        return [this.x(x), this.y(y)];
     }
 
     drawLine(x1, y1, x2, y2, width, color) {
-        this.ctx.beginPath();
-        this.ctx.moveTo(x1*F, y1*F);
-        this.ctx.lineTo(x2*F, y2*F);
-        this.ctx.lineWidth = width*F;
+        this.ctx.lineWidth = width;
         this.ctx.strokeStyle = color;
+        this.ctx.beginPath();
+        this.ctx.moveTo(...this.c(x1, y1));
+        this.ctx.lineTo(...this.c(x2, y2));
         this.ctx.stroke();
+        this.ctx.closePath()
     }
 
-    drawRect(x, y, width, height, fill_color, outline_width, outline_color) {
-        this.ctx.beginPath();
-        this.ctx.rect(x*F, y*F, width*F, height*F);
+    drawRect(x, y, width, height, fill_color, outline_color, outline_width) {
+        let args = [...this.c(x, y), ...this.c(width, height)]
         if (fill_color) {
             this.ctx.fillStyle = fill_color;
-            this.ctx.fill();
+            this.ctx.fillRect(...args);
         }
         if (outline_width && outline_color) {
-            this.ctx.lineWidth = outline_width*F;
+            this.ctx.lineWidth = outline_width;
             this.ctx.strokeStyle = outline_color;
-            this.ctx.stroke();
+            this.ctx.strokeRect(...args);
         }
     }
 
     drawCoordinateSystem(line_width) {
         this.clear();
 
-        let lw = line_width || 0.05; // line width
+        let lw = line_width || 1; // line width
         let c = 'rgba(255, 255, 255, 0.2)'; // color
 
-        for (let x = 1; x < this.width; x++) {
-            this.drawLine(x, 0, x, this.height, lw, c);
+        for (let x = 1; x < this.v_width; x++) {
+            this.drawLine(x, 0, x, this.v_height, lw, c);
         }
-        for (let y = 1; y < this.height; y++) {
-            this.drawLine(0, y, this.width, y, lw, c);
+        for (let y = 1; y < this.v_height; y++) {
+            this.drawLine(0, y, this.v_width, y, lw, c);
         }
 
-        this.drawRect(0, 0, this.width, this.height, null, 0.2, 'white');
+        this.drawRect(0, 0, this.v_width, this.v_height, null, 'white', 1);
     }
 }

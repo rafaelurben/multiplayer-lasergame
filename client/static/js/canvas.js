f127052f92858ed4e80680b7ce9adf71a4e6e4ee
rafaelurben <github@rafaelurben.ch>
2022-12-16 21:56:57 +0100

    Added GameCanvas.drawScore() method;

--- client/static/js/canvas.js
+++ client/static/js/canvas.js
@@ -6,7 +6,7 @@ class GameCanvas {
         this.mapHeight = mapHeight || 15;
 
         this.w = this.mapWidth + 1;
-        this.h = this.mapHeight + 3;
+        this.h = this.mapHeight + 2.5;
         this.ratio = this.w / this.h;
 
         this.stage = new Konva.Stage({
@@ -24,12 +24,12 @@ class GameCanvas {
 
         // Coordinate system layer
         this.coordlayer = this.getCoordinateSystemLayer();
-        this.coordlayer.move({ x: 0, y: 2, width: this.mapWidth + 1, height: this.mapHeight + 1 });
+        this.coordlayer.move({ x: 0, y: 1.5});
         this.stage.add(this.coordlayer);
 
         // Map layer
         this.maplayer = new Konva.Layer();
-        this.maplayer.move({ x: 0.5, y: 2.5 });
+        this.maplayer.move({ x: 0.5, y: 2 });
         this.stage.add(this.maplayer);
     }
 
@@ -58,6 +58,47 @@ class GameCanvas {
         this.stage.scale({ x: width / this.w, y: height / this.h });
     }
 
+    drawScore(score) {
+        let o = 0.5; // offset to avoid clipping outline
+        let css = window.getComputedStyle(document.documentElement);
+
+        this.scorelayer.destroyChildren();
+
+        let w = this.mapWidth;
+
+        let team0poly = new Konva.Line({
+            points: [
+                0, 0,
+                score * (w - 0.5) + 0.25, 0,
+                score * (w - 0.5), 1,
+                0, 1,
+            ],
+            fill: css.getPropertyValue('--col-team-0'),
+            stroke: 'black',
+            strokeWidth: 0.1,
+            closed: true,
+        })
+        team0poly.move({ x: o, y: o })
+        this.scorelayer.add(team0poly);
+
+        let team1poly = new Konva.Line({
+            points: [
+                w, 0,
+                score * (w - 0.5) + 0.5, 0,
+                score * (w - 0.5) + 0.25, 1,
+                w, 1,
+            ],
+            fill: css.getPropertyValue('--col-team-1'),
+            stroke: 'black',
+            strokeWidth: 0.1,
+            closed: true,
+        })
+        team1poly.move({ x: o, y: o })
+        this.scorelayer.add(team1poly);
+
+        this.scorelayer.draw();
+    }
+
     getCoordinateSystemLayer() {
         let layer = new Konva.Layer();
 
@@ -65,30 +106,36 @@ class GameCanvas {
         let c = 'rgba(255, 255, 255, 0.2)'; // color
 
         // Draw outline
-        layer.add(new Konva.Rect({
-            x: 0 + o,
-            y: 0 + o,
+        let outline = new Konva.Rect({
+            x: 0,
+            y: 0,
             width: this.mapWidth,
             height: this.mapHeight,
             stroke: 'white',
             strokeWidth: 0.1,
-        }));
+        });
+        outline.move({ x: o, y: o });
+        layer.add(outline);
 
         // Draw vertical lines
         for (let x = 1; x < this.mapWidth; x++) {
-            layer.add(new Konva.Line({
-                points: [x + o, 0 + o, x + o, this.mapHeight + o],
+            let line = new Konva.Line({
+                points: [x, 0, x, this.mapHeight],
                 stroke: c,
                 strokeWidth: 0.01,
-            }));
+            });
+            line.move({ x: o, y: o });
+            layer.add(line);
         }
         // Draw horizontal lines
         for (let y = 1; y < this.mapHeight; y++) {
-            layer.add(new Konva.Line({
-                points: [0 + o, y + o, this.mapWidth + o, y + o],
+            let line = new Konva.Line({
+                points: [0, y, this.mapWidth, y],
                 stroke: c,
                 strokeWidth: 0.01,
-            }));
+            });
+            line.move({ x: o, y: o });
+            layer.add(line);
         }
 
         return layer;
